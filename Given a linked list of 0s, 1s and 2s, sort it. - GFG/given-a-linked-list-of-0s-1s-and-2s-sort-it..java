50d60a1fb3c956cb9b404a0e48fb47d33becccaf
VASAVI <97425195+21A91A05G9@users.noreply.github.com>
2023-07-23 20:05:28 +0530

    Added solution - LeetHub

---
+++ Given a linked list of 0s, 1s and 2s, sort it. - GFG/given-a-linked-list-of-0s-1s-and-2s-sort-it..java
@@ -0,0 +1,123 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class Driverclass
+{
+    public static void main (String[] args) throws IOException
+    {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t = Integer.parseInt(in.readLine().trim());
+        
+        while(t-- > 0)
+        {
+            int n =Integer.parseInt(in.readLine().trim());
+            String s[] = in.readLine().trim().split(" ");
+            Node head = new Node(Integer.parseInt(s[0]));
+            Node tail = head;
+            for (int i = 1; i < n; i++) {
+                tail.next = new Node(Integer.parseInt(s[i]));
+                tail = tail.next;
+            }
+
+            head = new Solution().segregate(head);
+            printList(head, out);
+            out.println();
+        }
+        out.close();
+    }
+    
+    public static void printList(Node head,PrintWriter out)
+    {
+        if(head == null)
+           return;
+           
+        Node temp = head;
+        while(temp != null)
+        {
+            out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+    
+    
+}
+
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int data)
+    {
+        this.data = data;
+        next = null;
+    }
+}
+*/
+class Solution
+{
+    //Function to sort a linked list of 0s, 1s and 2s.
+    static Node segregate(Node head)
+    {
+        // add your code here
+        Node p=new Node(0);
+        Node q=new Node(0);
+        Node r=new Node(0);
+        Node t=head,a=p,b=q,c=r;
+        while(t!=null){
+            if(t.data==0){
+                Node n=t.next;
+                t.next=null;
+                a.next=t;
+                a=a.next;
+                t=n;
+                continue;
+            }
+            else if(t.data==1){
+                Node n=t.next;
+                t.next=null;
+                b.next=t;
+                b=b.next;
+                t=n;
+                continue;
+            }
+            else{
+                Node n=t.next;
+                t.next=null;
+                c.next=t;
+                c=c.next;
+                t=n;
+                continue;
+            }
+        }
+        b.next=r.next;
+        a.next=q.next;
+        return p.next;
+    }
+}
+
+
