3b0a2cce383952f6db1c26830b99fb43992b9f27
VASAVI <97425195+21A91A05G9@users.noreply.github.com>
2024-02-25 22:57:13 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Reach a given score/reach-a-given-score.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+// Driver Code
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+   long long f(int ind, vector<int> & vals, long long n, vector<vector<int>> & dp){
+        if(ind<0) return 0;
+        if(n==0) return 1;
+        if(dp[ind][n]!=-1) return dp[ind][n];
+        int p=0, np=0;
+        if(vals[ind]<=n){
+            p=f(ind,vals,n-vals[ind], dp);
+        }
+        np=f(ind-1,vals,n,dp);
+        return dp[ind][n]=p+np;
+    }
+    long long int count(long long int n)
+    {
+        vector<int> vals={3,5,10};
+        vector<vector<int>> dp(3, vector<int>(n+1,-1));
+        return f(2,vals,n,dp);
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		ll n;
+		cin>>n;
+		Solution obj;
+		cout<<obj.count(n)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
