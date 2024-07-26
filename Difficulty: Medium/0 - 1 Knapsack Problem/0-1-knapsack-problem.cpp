86a14f7d62c7fab2770cae25acc8dde354c5b86a
VASAVI <97425195+21A91A05G9@users.noreply.github.com>
2024-07-26 16:09:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -7,29 +7,22 @@ using namespace std;
 class Solution
 {
     public:
-    int solve(int i, vector<pair<double,pair<int,int>>>&v, int w, vector<vector<int>>&dp) {
+    //Function to return max value that can be put in knapsack of capacity W.
+    int solve(int i, int w, int wt[], int val[], vector<vector<int>>&dp) {
         if(i < 0) return 0;
-        if(w < 0) return 0;
-        int p=0, np=0;
         if(dp[i][w] != -1) return dp[i][w];
-        if((w-v[i].second.first) >= 0) p = v[i].second.second + solve(i-1, v, w-v[i].second.first, dp);
-        np = solve(i-1, v, w, dp);
+        int p=0, np=0;
+        
+        if((w-wt[i]) >= 0) p  = val[i] + solve(i-1, w-wt[i], wt, val, dp);
+        np = solve(i-1, w, wt, val, dp);
+        
         return dp[i][w] = max(p, np);
     }
-    //Function to return max value that can be put in knapsack of capacity W.
     int knapSack(int W, int wt[], int val[], int n) 
     { 
        // Your code here
-       vector<pair<double,pair<int,int>>>v;
-       vector<vector<int>>dp(n+1,vector<int>(W+1, -1));
-       
-       for(int i=0; i<n; i++) {
-           double frac = (val[i]/(1.0*wt[i]));
-           v.push_back({frac,{wt[i], val[i]}});
-       }
-       sort(v.begin(), v.end());
-       
-       return solve(n-1, v, W, dp);
+       vector<vector<int>>dp(n, vector<int>(W+1, -1));
+       return solve(n-1, W, wt, val, dp);
     }
 };
 
