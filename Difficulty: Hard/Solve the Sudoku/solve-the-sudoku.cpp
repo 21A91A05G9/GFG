d55b043f5b912bd3e4e0238d5a934bad2c22a855
VASAVI <97425195+21A91A05G9@users.noreply.github.com>
2024-07-27 10:00:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Solve the Sudoku/solve-the-sudoku.cpp
+++ Difficulty: Hard/Solve the Sudoku/solve-the-sudoku.cpp
@@ -13,24 +13,27 @@ using namespace std;
 class Solution 
 {
     public:
-    //Function to find a solved Sudoku.
+    
+    //Function to find a solved Sudoku
     bool isSafe(int i, int j, int k, int grid[N][N]) {
         for(int ind=0; ind<9; ind++) {
-            if(grid[i][ind] == k) return false;
-            if(grid[ind][j] == k) return false;
+            if(grid[i][ind] == k or grid[ind][j] == k) return false;
         }
-            
+        
         int l=(i/3)*3, r=(j/3)*3;
+        
         for(int ind1=0; ind1<3; ind1++) {
             for(int ind2=0; ind2<3; ind2++) {
-                if(grid[ind1+l][ind2+r] == k) return false;
+                if(grid[l+ind1][r+ind2] == k) return false;
             }
         }
         return true;
     }
-    bool solve(int i, int j, int grid[N][N]) {
+    
+    bool solve(int i, int j, int grid[N][N]){
         if(i == 9) return true;
         if(j == 9) return solve(i+1, 0, grid);
+        
         if(grid[i][j] == 0) {
             for(int k=1; k<=9; k++) {
                 if(isSafe(i, j, k, grid)) {
@@ -39,27 +42,27 @@ class Solution
                     grid[i][j] = 0;
                 }
             }
+            return false;
         }
         else {
             if(solve(i, j+1, grid)) return true;
+            return false;
         }
-        return false;
-        
     }
     bool SolveSudoku(int grid[N][N])  
     { 
-        // Your code he
         return solve(0, 0, grid);
     }
     
     //Function to print grids of the Sudoku.
     void printGrid (int grid[N][N]) 
     {
-       for(int i=0; i<9; i++) {
-           for(int j=0; j<9; j++) {
-               cout<<grid[i][j]<<" ";
-           }
-       }
+        // Your code here 
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<N; j++) {
+                cout<<grid[i][j]<<" ";
+            }
+        }
     }
 };
 
