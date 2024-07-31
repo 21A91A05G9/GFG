753214bdb8fe38a6b442864702ce21c98796e280
VASAVI <97425195+21A91A05G9@users.noreply.github.com>
2024-07-31 18:40:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Deletions/minimum-deletions.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    int minimumNumberOfDeletions(string s) { 
+        // code here
+        string t;
+        for(auto e: s) t = e+t;
+        vector<vector<int>>dp(s.size()+1, vector<int>(s.size()+1, 0));
+        for(int i=1; i<=s.size(); i++) {
+            for(int j=1; j<=s.size(); j++) {
+                if(s[i-1] == t[j-1]) dp[i][j] = 1+dp[i-1][j-1];
+                else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        
+        return s.size()-dp[s.size()][s.size()];
+    } 
+};
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        string S;
+        cin >> S;
+        Solution obj;
+        cout << obj.minimumNumberOfDeletions(S) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
